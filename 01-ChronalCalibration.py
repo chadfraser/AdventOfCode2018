a45caffe3940b4f85530b0db49836573b40b6899
Chad Fraser <chad.fraser@mail.mcgill.ca>
2018-12-11 19:47:18 -0700

    Simplify code and add docstrings for days 1 and 2

--- 01-ChronalCalibration.py
+++ 01-ChronalCalibration.py
@@ -1,24 +1,41 @@
 def sum_input_file(file):
+    """Given a file with an int on each line, returns the total sum of those ints
+
+    :param file: The text file from which we take all of the ints we want to sum
+    :return: The total sum of all ints in our file
+    """
     total_sum = 0
     for line in file:
         total_sum += int(line.strip())
-    print(total_sum)
+    return total_sum
 
 
 def find_first_repeated_sum(file):
+    """Given a file with an int on each line, sum every int in turn and store that sum.
+    Then, return once we reach a sum that we've already stored.
+    If we reach the end of file without reaching a sum that we've already stored, restart from the beginning of the
+    file again without resetting our sum to 0.
+
+    :param file: The text file from which we take all of the ints we want to sum
+    :return: The sum
+    """
     total_sum = 0
     found_sums = set()
+
     while True:
         for line in file:
             total_sum += int(line.strip())
             if total_sum in found_sums:
-                print(total_sum)
                 return total_sum
             found_sums.add(total_sum)
         input_file.seek(0)
 
 
-with open("01-ChronalCalibration-Input.txt") as input_file:
-    sum_input_file(input_file)
-    input_file.seek(0)
-    find_first_repeated_sum(input_file)
+if __name__ == "__main__":
+    with open("01-ChronalCalibration-Input.txt") as input_file:
+        file_sum = sum_input_file(input_file)
+        print(file_sum)
+
+        input_file.seek(0)
+        first_repeated_sum = find_first_repeated_sum(input_file)
+        print(first_repeated_sum)
--- 02-InventoryManagementSystem.py
+++ 02-InventoryManagementSystem.py
@@ -1,7 +1,14 @@
 from collections import Counter
 
 
-def count_repeated_letters(file):
+def count_doubled_and_tripled_letters(file):
+    """Finds the count of all letters in each line of our file.
+    Stores and returns the amount of lines that contain exactly two of one or more letter(s), and the amount of lines
+    that contain exactly three of one or more letter(s).
+
+    :param file: The file from which we take all of the strings that we want to count the repeated letters of
+    :return: The amount of lines that contain exactly two/three of one or more letter(s)
+    """
     doubles = 0
     triples = 0
     for line in file:
@@ -10,33 +17,64 @@ def count_repeated_letters(file):
             doubles += 1
         if 3 in counted_line.values():
             triples += 1
-    print(doubles, triples, doubles * triples)
+    return doubles, triples
 
 
 def find_similar_strings(file):
+    """Finds the two lines in our file that differ by only one character in one position.
+
+    :param file: The file from which we take all of the strings that we want to compare
+    :return: The lines that differ by only one character in one position
+    """
     list_of_checked_lines = []
     for line in file:
-        for item in list_of_checked_lines:
-            if compare_differences_in_strings(line.strip(), item):
-                print(line.strip(), item)
-                for line_char, item_char in zip(line, item):
-                    if line_char == item_char:
-                        print(line_char, end="")
-                return
+        line = line.strip()
+        for line_to_compare in list_of_checked_lines:
+            if compare_differences_in_strings(line, line_to_compare):
+                return line, line_to_compare
         list_of_checked_lines.append(line.strip())
 
 
-def compare_differences_in_strings(line, item):
+def compare_differences_in_strings(string_a, string_b):
+    """Compare all of the letters in two strings.
+    Returns whether the two strings differ by only one or zero character(s)
+
+    :param string_a: The first string that we want to compare
+    :param string_b: The second string that we want to compare
+    :return: A boolean saying if the two strings differ by only one or zero character(s)
+    """
     differences = 0
-    for line_char, item_char in zip(line, item):
-        if line_char != item_char:
+    for char_a, char_b in zip(string_a, string_b):
+        if char_a != char_b:
             differences += 1
             if differences > 1:
                 return False
     return True
 
 
-with open("02-InventoryManagementSystem-Input.txt") as input_file:
-    count_repeated_letters(input_file)
-    input_file.seek(0)
-    find_similar_strings(input_file)
+def get_shared_string_characters(string_a, string_b):
+    """Finds the string made by removing all characters that differ in our two strings parameters.
+
+    :param string_a: The first string that we want to compare
+    :param string_b: The second string that we want to compare
+    :return: The string made by removing all characters that differ in our two strings parameters
+    """
+    # shared_string = ""
+    shared_string_list = [char_a for char_a, char_b in zip(string_a, string_b) if char_a == char_b]
+    # for char_a, char_b in zip(string_a, string_b):
+    #     if char_a == char_b:
+    #         shared_string += char_a
+    return "".join(shared_string_list)
+
+
+if __name__ == "__main__":
+    with open("02-InventoryManagementSystem-Input.txt") as input_file:
+        doubled_values, tripled_values = count_doubled_and_tripled_letters(input_file)
+        print(doubled_values, tripled_values, doubled_values * tripled_values)
+
+        input_file.seek(0)
+        similar_string_a, similar_string_b = find_similar_strings(input_file)
+        print(similar_string_a, similar_string_b)
+
+        similar_string_characters = get_shared_string_characters(similar_string_a, similar_string_b)
+        print(similar_string_characters)
